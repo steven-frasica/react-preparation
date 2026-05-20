ff9c38a73132176deedd7697ac113f80f73d086b
steven-frasica <steven.frasica@gmail.com>
2026-05-20 11:44:21 -0400

    Finish Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  let navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+
+  async function getUsers() {
+    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          {users.map((user) => (<div className="user" key={user.id} onClick={() => navigate(`${user.id}`)}>
+            <div className="user-card">
+              <div className="user-card__container">
+                <h3>{user.name}</h3>
+                <p>
+                  <b>Email: </b> {user.email}
+                </p>
+                <p>
+                  <b>Phone:</b>{user.phone}
+                </p>
+                <p>
+                  <b>Website: </b>
+                  {user.website}
+                </p>
+              </div>
+            </div>
+          </div>))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,19 +1,75 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const Posts = () => {
+
+  let navigate = useNavigate();
   const { id } = useParams();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState();
+  const [searchId, setSearchId] = useState(id);
+
+  function onSearch() {
+    console.log(searchId);
+    fetchPosts(searchId);
+  }
+
+  async function fetchPosts(userId) {
+    setLoading(true);
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`,
+    );
+    setPosts(data);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }
+
+  function onSearchKeyPress(key) {
+    if (key === "Enter") {
+      onSearch();
+    }
+  }
 
   useEffect(() => {
-    async function fetchPosts() {
-      const data = fetch("https://jsonplaceholder.typicode.com/posts/1")
-      console.log('mounted', data)
-    }
-  },[])
+    fetchPosts();
+  }, []);
 
   return (
-    <div>{id}</div>
-  )
-}
+    <>
+      <div className="post__search">
+          <button onClick={() => navigate('/')}>← Back</button>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+          <input
+            type="number"
+            value={searchId}
+            onKeyPress={(event) => onSearchKeyPress(event.key)}
+            onChange={(event) => setSearchId(event.target.value)}
+          />
+          <button onClick={() => onSearch()}>Enter</button>
+        </div>
+      </div>
+      {loading
+        ? posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="post__title">
+                <div className="post__title--skeleton"></div>
+              </div>
+              <div className="post__body">
+                <p className="post__body--skeleton"></p>
+              </div>
+            </div>
+          ))
+        : posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="post__title">{post.title}</div>
+              <p className="post__body">{post.body}</p>
+            </div>
+          ))}
+    </>
+  );
+};
 
-export default Posts
+export default Posts;
